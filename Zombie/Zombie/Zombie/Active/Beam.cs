e62ec7645da2fd03e1a309d90095be91a99ee8ae
Michael Holmes <Michael Holmes>
2016-07-21 16:55:24 +0900

    プロパティーをBeamクラスに追加（まだ使っていません）

--- Zombie/Zombie/Zombie/Active/Beam.cs
+++ Zombie/Zombie/Zombie/Active/Beam.cs
@@ -12,20 +12,64 @@ namespace Zombie.Active
         private Vector2 position;
         private Vector2 velocity;
         private Vector2 size;
+        private float range;
 
         private float start;    //発射した時のX座標
+        private Vector2 startPos;
         private int rf;            //プレイヤーの向き
         private bool beamType;
 
-        public Beam(Vector2 position, Vector2 size, Vector2 velocity, bool beamType)
-             {
-                name = "beam";
-                this.beamType = beamType;
-                this.position = position;
-                this.velocity = velocity;
-                this.size = size;
+        //プロパティー
+        public Vector2 Position
+        {
+            get { return position; }
         }
 
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+        }
+
+        public Vector2 Size
+        {
+            get { return size; }
+        }
+
+        public float DistanceTravelled
+        {
+            get { return Vector2.Distance(startPos, position); }
+        }
+
+        public float Range
+        {
+            get { return range; }
+        }
+
+        public float Start
+        {
+            get { return start; }
+            set { start = value; }
+        }
+
+        public int RightFacing
+        {
+            get { return rf; }
+            set { rf = value; }
+        }
+
+        //コンストラクタ
+        public Beam(Vector2 position, Vector2 size, Vector2 velocity, bool beamType)
+        {
+            name = "beam";
+            this.beamType = beamType;
+            this.position = position;
+            this.velocity = velocity;
+            this.size = size;
+            startPos = position;
+            range = 500;
+        }
+
+        //メソッド
         public void Update()
         {
             velocity.X += rf;
